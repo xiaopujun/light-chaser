05e5b9429b89c47d96f92a5b93c5a52ee996e47a
xiaopu <1182810784@qq.com>
2022-10-24 22:53:24 +0800

    去除redux开发工具

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,4 @@
 import {createStore} from 'redux';
 import reducer from './reducers';
-//引入开发者工具
-import {composeWithDevTools} from 'redux-devtools-extension';
 
-export default createStore(reducer, composeWithDevTools());
+export default createStore(reducer);
