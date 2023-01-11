66e0ce8a3195d4619c7545daec4070ae6e9616ab
xiaopu <1182810784@qq.com>
2023-01-11 21:52:50 +0800

    改版UI

--- src/component/base/LcUnderLineInput.tsx
+++ src/component/base/LcUnderLineInput.tsx
@@ -2,21 +2,35 @@ import React, {Component} from 'react';
 import './style/LcUnderLineInput.less';
 
 interface LcUnderLineInputProps {
+    name?: string;
     type?: string;
+    value?: string | number;
     containStyle?: any;
     inputStyle?: any;
     lineStyle?: any;
+    onChange?: (e: any) => void;
 }
 
 /**
  * 下滑线输入框
  */
 class LcUnderLineInput extends Component<LcUnderLineInputProps> {
+
+    onChange = (e: any) => {
+        const {onChange} = this.props;
+        onChange && onChange(e);
+    }
+
     render() {
-        const {type = 'text', containStyle, inputStyle, lineStyle} = this.props;
+        const {type = 'text', containStyle, inputStyle, lineStyle, name, value} = this.props;
         return (
             <div className={'lc-underline-input-container'} style={containStyle}>
-                <input className={'lc-underline-input'} type={type} style={inputStyle}/>
+                <input name={name}
+                       type={type}
+                       value={value}
+                       onChange={this.onChange}
+                       className={'lc-underline-input'}
+                       style={inputStyle}/>
                 <span className={'lc-underline-span'} style={lineStyle}/>
             </div>
 
--- src/component/config/base/PaddingSet.tsx
+++ src/component/config/base/PaddingSet.tsx
@@ -12,70 +12,123 @@ interface PaddingSetProps {
 
 export default class PaddingSet extends Component<PaddingSetProps> {
 
+    private static ALONE = 'alone';
+    private static TOP_BOTTOM = 'top-bottom';
+    private static LEFT_RIGHT = 'left-right';
+    private static ALL = 'all';
+
     state: any = {
-        data: []
+        mode: [PaddingSet.ALONE, PaddingSet.TOP_BOTTOM, PaddingSet.LEFT_RIGHT, PaddingSet.ALL],
+        modeIndex: 0,
+        dataArr: [0, 0, 0, 0],
+        data: ''
     }
 
     constructor(props: PaddingSetProps) {
         super(props);
-        const {value} = this.props;
-        if (value == null) {
-            this.state = {data: [0, 0, 0, 0]};
-            return;
-        }
-        let paddingItem = value.split(' ');
-        let initArr = [];
-        for (let i = 0; i < paddingItem.length; i++)
-            initArr.push(parseInt(paddingItem[i].replace('px', '')));
-        this.state = {data: initArr};
     }
 
 
-    onChange = (value: number, id: string) => {
-        let {data} = this.state;
-        const {onChange} = this.props;
-        data[id] = value;
-        this.setState({data});
-        let paddingSet = "";
-        for (let i = 0; i < data.length; i++)
-            paddingSet += data[i] + 'px ';
-        paddingSet = paddingSet.trim();
-        onChange && onChange(paddingSet);
+    onChange = (e: any) => {
+        let name = e.currentTarget.name;
+        let value = parseInt(e.currentTarget.value);
+        switch (name) {
+            case 'top':
+                this.calculateModeValue(0, value, name);
+                break;
+            case 'bottom':
+                this.calculateModeValue(2, value, name);
+                break;
+            case 'left':
+                this.calculateModeValue(3, value, name);
+                break;
+            case 'right':
+                this.calculateModeValue(1, value, name);
+                break;
+            default:
+        }
+    }
+
+    calculateModeValue = (index: number, value: number, position: string) => {
+        const {modeIndex, mode, dataArr} = this.state;
+        let currentMode = mode[modeIndex];
+        switch (currentMode) {
+            case PaddingSet.TOP_BOTTOM:
+                if (currentMode.indexOf(position) > -1) {
+                    dataArr[0] = value;
+                    dataArr[2] = value;
+                } else {
+                    dataArr[index] = value;
+                }
+                break;
+            case PaddingSet.LEFT_RIGHT:
+                if (currentMode.indexOf(position) > -1) {
+                    dataArr[1] = value;
+                    dataArr[3] = value;
+                } else {
+                    dataArr[index] = value;
+                }
+                break;
+            case PaddingSet.ALL:
+                dataArr[0] = value;
+                dataArr[1] = value;
+                dataArr[2] = value;
+                dataArr[3] = value;
+                break;
+            default:
+                dataArr[index] = value;
+        }
+        this.setState({dataArr: [...dataArr]});
+    }
+
+    modeChange = () => {
+        let {mode, modeIndex} = this.state;
+        modeIndex++;
+        if (modeIndex === mode.length)
+            modeIndex = 0;
+        this.setState({modeIndex});
     }
 
     render() {
-        const {unit = 'px'} = this.props!;
-        const {data} = this.state;
-        let itemArr = [];
-        for (let i = 0; i < data.length; i++) {
-            itemArr.push(
-                <span className={'lc-input-container'} key={i + ''}>
-                    <LCNumberInput id={i + ""} value={data[i]} onChange={this.onChange}/>
-                    <label>&nbsp;{unit}</label>
-                </span>
-            )
-        }
+        const {dataArr, mode, modeIndex} = this.state;
         return (
             <div className={'lc-padding-config'}>
                 <div className={'lc-padding-content'}>
                     <div className={'lc-padding-top'}>
-                        <LcUnderLineInput type={'number'} inputStyle={{width: 20}}
-                                          containStyle={{width: 20}}/><span>px</span>
+                        <LcUnderLineInput type={'number'}
+                                          value={dataArr[0]}
+                                          name={'top'}
+                                          onChange={this.onChange}
+                                          inputStyle={{width: 25, textAlign: 'center'}}
+                                          containStyle={{width: 25}}/><span>px</span>
                     </div>
                     <div className={'lc-padding-middle'}>
                         <div className={'lc-padding-left'}>
-                            <LcUnderLineInput type={'number'} inputStyle={{width: 20}}
-                                              containStyle={{width: 20}}/><span>px</span>
+                            <LcUnderLineInput type={'number'}
+                                              name={'left'}
+                                              value={dataArr[3]}
+                                              onChange={this.onChange}
+                                              inputStyle={{width: 25, textAlign: 'center'}}
+                                              containStyle={{width: 25}}/><span>px</span>
                         </div>
-                        <div className={'lc-padding-center'}>alone</div>
+                        <div className={'lc-padding-center'} onClick={this.modeChange}>{mode[modeIndex]}</div>
                         <div className={'lc-padding-right'}>
-                            <LcUnderLineInput type={'number'} inputStyle={{width: 20}}
-                                              containStyle={{width: 20}}/><span>px</span>
+                            <LcUnderLineInput name={'right'}
+                                              type={'number'}
+                                              value={dataArr[1]}
+                                              onChange={this.onChange}
+                                              inputStyle={{width: 25, textAlign: 'center'}}
+                                              containStyle={{width: 25}}/><span>px</span>
                         </div>
                     </div>
                     <div className={'lc-padding-bottom'}>
-                        <LcUnderLineInput type={'number'} inputStyle={{width: 20}}
-                                          containStyle={{width: 20}}/><span>px</span>
+                        <LcUnderLineInput type={'number'}
+                                          name={'bottom'}
+                                          value={dataArr[2]}
+                                          onChange={this.onChange}
+                                          inputStyle={{width: 25, textAlign: 'center'}}
+                                          containStyle={{width: 25}}/>
+                        <span>px</span>
                     </div>
                 </div>
 
