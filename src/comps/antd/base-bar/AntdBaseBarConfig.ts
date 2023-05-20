4ce397cd1a6baa0ec72126aca3104caa24044e32
xiaopu <1182810784@qq.com>
2023-05-20 16:29:08 +0800

    动画设置验证

--- src/comps/antd/base-bar/AntdBaseBarConfig.ts
+++ src/comps/antd/base-bar/AntdBaseBarConfig.ts
@@ -4,9 +4,9 @@ import {ClassType} from "react";
 import BaseInfo from "../../../lib/common-fragment/base-info/BaseInfo";
 import {MenuInfo} from "../../../framework/types/MenuType";
 import AntdBaseBarConfigStyle from "./AntdBaseBarConfigStyle";
-import AntdBaseBarConfigAnimation from "./AntdBaseBarConfigAnimation";
 import DataConfig from "../../../lib/common-fragment/data-config/DataConfig";
 import ThemeConfig from "../../../lib/common-fragment/theme-config/ThemeConfig";
+import AnimationConfig from "../../../lib/common-fragment/animation-config/AnimationConfig";
 
 export default class AntdBaseBarConfig extends AbstractConfig {
     getMenuList(): Array<MenuInfo> {
@@ -18,7 +18,7 @@ export default class AntdBaseBarConfig extends AbstractConfig {
             'info': BaseInfo,
             'style': AntdBaseBarConfigStyle,
             'data': DataConfig,
-            'animation': AntdBaseBarConfigAnimation,
+            'animation': AnimationConfig,
             'theme': ThemeConfig
         };
     }
---
+++ src/lib/common-fragment/animation-config/AnimationConfig.tsx
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {Button} from "antd";
+import ConfigCard from "../../config-card/ConfigCard";
+import ConfigItem from "../../config-item/ConfigItem";
+import NumberInput from "../../lc-input/NumberInput";
+import LcSwitch from "../../lc-switch/LcSwitch";
+import 'animate.css';
+import './AnimationConfig.less';
+
+class AnimationConfig extends Component {
+    render() {
+        return (
+            <div className={'lc-animation-config'}>
+                <Button disabled={true} type="primary" style={{width: '100%'}}>清除动画</Button>
+                <ConfigCard title={'动画配置'}>
+                    <ConfigItem title={'效果'}>
+                        <div style={{color: '#b2b2b2', fontSize: 12}}>向上滑动</div>
+                    </ConfigItem>
+                    <ConfigItem title={'时间'}>
+                        <NumberInput defaultValue={1}/>
+                    </ConfigItem>
+                    <ConfigItem title={'延迟'}>
+                        <NumberInput defaultValue={1}/>
+                    </ConfigItem>
+                    <ConfigItem title={'次数'}>
+                        <NumberInput defaultValue={1}/>
+                    </ConfigItem>
+                    <ConfigItem title={'循环'}>
+                        <LcSwitch/>
+                    </ConfigItem>
+                </ConfigCard>
+                <ConfigCard title={'动画列表'}>
+                    <div className={'animation-item animate__animated animate__bounce'}>bounce</div>
+                    <div className={'animation-item animate__animated animate__flash'}>flash</div>
+                    <div className={'animation-item animate__animated animate__pulse'}>pulse</div>
+                    <div className={'animation-item animate__animated animate__fadeIn'}>fadeIn</div>
+                    <div className={'animation-item animate__animated animate__fadeInDown'}>fadeInDown</div>
+                    <div className={'animation-item animate__animated animate__fadeInLeft'}>fadeInLeft</div>
+                    <div className={'animation-item animate__animated animate__fadeInRight'}>fadeInRight</div>
+                    <div className={'animation-item animate__animated animate__fadeInUp'}>fadeInUp</div>
+                    <div className={'animation-item animate__animated animate__slideInDown'}>slideInDown</div>
+                    <div className={'animation-item animate__animated animate__slideInLeft'}>slideInLeft</div>
+                    <div className={'animation-item animate__animated animate__slideInRight'}>slideInRight</div>
+                    <div className={'animation-item animate__animated animate__slideInUp'}>slideInUp</div>
+                </ConfigCard>
+            </div>
+        );
+    }
+}
+
+export default AnimationConfig;
