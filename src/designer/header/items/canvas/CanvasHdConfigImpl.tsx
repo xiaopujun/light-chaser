2b952154d3d251971b1bcd12f210b6b3b8e6bd3a
xiaopu <1182810784@qq.com>
2023-07-07 14:13:20 +0800

    要是调整及数据校验

--- src/designer/header/items/canvas/CanvasHdConfigImpl.tsx
+++ src/designer/header/items/canvas/CanvasHdConfigImpl.tsx
@@ -47,42 +47,41 @@ class CanvasHdConfigImpl extends Component {
         const {width, height, rasterize, dragStep, resizeStep} = this.config as CanvasConfig;
         return (
             <Dialog className={'lc-header-canvas'} title={'画布设置'} visible={canvasVisible} onClose={this.onClose}>
-                <div style={{display: 'flex', flexWrap: 'wrap'}}>
-                    <ConfigItem title={'宽度'} contentStyle={{width: 110}}>
-                        <UnderLineInput type={'number'} defaultValue={width}
-                                        onChange={value => this.config.width = value}/>
-                    </ConfigItem>
-                    <ConfigItem title={'高度'} contentStyle={{width: 110}}>
-                        <UnderLineInput type={'number'} defaultValue={height}
-                                        onChange={value => this.config.height = value}/>
-                    </ConfigItem>
-                    <ConfigItem title={'栅格化'} contentStyle={{width: 60}}>
-                        <LcSwitch defaultValue={rasterize} onChange={value => {
-                            this.config.rasterize = value;
-                            this.setState({_rasterize: value});
-                        }}/>
-                    </ConfigItem>
-                    {_rasterize && <>
+                <form onSubmit={this.doSave}>
+                    <div style={{display: 'flex', flexWrap: 'wrap'}}>
+                        <ConfigItem title={'宽度'} contentStyle={{width: 110}}>
+                            <UnderLineInput type={'number'} defaultValue={width}
+                                            onChange={value => this.config.width = value} required={true} min={500}/>
+                        </ConfigItem>
+                        <ConfigItem title={'高度'} contentStyle={{width: 110}}>
+                            <UnderLineInput type={'number'} defaultValue={height}
+                                            onChange={value => this.config.height = value} required={true} min={300}/>
+                        </ConfigItem>
+                        <ConfigItem title={'栅格化'} contentStyle={{width: 60}}>
+                            <LcSwitch defaultValue={rasterize} onChange={value => {
+                                this.config.rasterize = value;
+                                this.setState({_rasterize: value});
+                            }}/>
+                        </ConfigItem>
                         <ConfigItem title={'拖拽步长'} contentStyle={{width: 60}}>
-                            <UnderLineInput type={'number'} defaultValue={dragStep}
+                            <UnderLineInput readOnly={!_rasterize} type={'number'} defaultValue={dragStep}
                                             onChange={value => this.config.dragStep = value}/>
                         </ConfigItem>
                         <ConfigItem title={'缩放步长'} contentStyle={{width: 60}}>
-                            <UnderLineInput type={'number'} defaultValue={resizeStep}
+                            <UnderLineInput readOnly={!_rasterize} type={'number'} defaultValue={resizeStep}
                                             onChange={value => this.config.resizeStep = value}/>
-                        </ConfigItem></>
-                    }
-
-                </div>
-                <p style={{
-                    color: '#7c7c7c',
-                    padding: '0 5px',
-                    fontSize: '12px'
-                }}>说明：修改画布设置，会对整体效果产生较大影响，建议先调试好画布设置后再进行大屏设计</p>
-                <div className={'lc-header-canvas-footer'}>
-                    <LcButton onClick={this.doSave}>保存</LcButton>
-                    <LcButton onClick={this.onClose}>取消</LcButton>
-                </div>
+                        </ConfigItem>
+                    </div>
+                    <p style={{
+                        color: '#7c7c7c',
+                        padding: '0 5px',
+                        fontSize: '12px'
+                    }}>说明：修改画布设置，会对整体效果产生较大影响，建议先调试好画布设置后再进行大屏设计</p>
+                    <div className={'lc-header-canvas-footer'}>
+                        <LcButton type={'submit'}>保存</LcButton>
+                        <LcButton type={'button'} onClick={this.onClose}>取消</LcButton>
+                    </div>
+                </form>
             </Dialog>
         );
     }
--- src/designer/header/items/project/ProjectHdItemImpl.tsx
+++ src/designer/header/items/project/ProjectHdItemImpl.tsx
@@ -29,7 +29,8 @@ class ProjectHdItemImpl extends Component {
         setProjectVisible(false);
     }
 
-    doSave = () => {
+    doSave = (e: any) => {
+        e.preventDefault();
         const {name, state, saveType} = this.config;
         if (name === '')
             alert('项目名称不能为空');
@@ -47,36 +48,40 @@ class ProjectHdItemImpl extends Component {
         const {projectConfig: {name, des, state, saveType, realTimeRefresh}} = designerStore;
         return (
             <Dialog title={'项目设置'} className={'lc-header-project-set'} visible={projectVisible} onClose={this.onClose}>
-                <div style={{display: 'flex', flexWrap: 'wrap'}}>
-                    <ConfigItem title={'项目名称'} contentStyle={{width: 120}}>
-                        <UnderLineInput defaultValue={name} onChange={(value) => this.config.name = value}/>
-                    </ConfigItem>
-                    <ConfigItem title={'项目描述'} contentStyle={{width: 140}}>
-                        <UnderLineInput defaultValue={des} onChange={(value) => this.config.des = value}/>
-                    </ConfigItem>
-                    <ConfigItem title={'项目状态'} contentStyle={{width: 190}}>
-                        <Radio onChange={value => this.config.state = value} defaultValue={state} options={[
-                            {label: '草稿', value: '0'},
-                            {label: '发布', value: '1'},
-                            {label: '封存', value: '2'}
-                        ]}/>
-                    </ConfigItem>
-                    <ConfigItem title={'存储类型'} contentStyle={{width: 190}}>
-                        <Radio onChange={value => this.config.saveType = value} defaultValue={saveType} options={[
-                            {label: '本地', value: '0'},
-                            // {label: '服务端', value: '1'},
-                        ]}/>
-                    </ConfigItem>
-                    <ConfigItem title={'草稿状态数据实时刷新'} contentStyle={{width: 190}}>
-                        <LcSwitch defaultValue={realTimeRefresh}
-                                  onChange={value => this.config.realTimeRefresh = value}/>
-                    </ConfigItem>
-                </div>
-                <p style={{padding: '5px 7px', color: '#989898'}}>说明：数据实时刷新开启后，草稿状态下组件数据也会根据配置实时更新</p>
-                <div className={'lc-header-project-footer'}>
-                    <LcButton onClick={this.doSave}>保存</LcButton>
-                    <LcButton onClick={this.onClose}>取消</LcButton>
-                </div>
+                <form onSubmit={this.doSave}>
+                    <div style={{display: 'flex', flexWrap: 'wrap'}}>
+                        <ConfigItem title={'项目名称'} contentStyle={{width: 120}}>
+                            <UnderLineInput defaultValue={name} onChange={(value) => this.config.name = value}
+                                            required={true} maxLength={20}/>
+                        </ConfigItem>
+                        <ConfigItem title={'项目描述'} contentStyle={{width: 140}}>
+                            <UnderLineInput defaultValue={des} onChange={(value) => this.config.des = value}
+                                            maxLength={60}/>
+                        </ConfigItem>
+                        <ConfigItem title={'项目状态'} contentStyle={{width: 190}}>
+                            <Radio onChange={value => this.config.state = value} defaultValue={state} options={[
+                                {label: '草稿', value: '0'},
+                                {label: '发布', value: '1'},
+                                {label: '封存', value: '2'}
+                            ]}/>
+                        </ConfigItem>
+                        <ConfigItem title={'存储类型'} contentStyle={{width: 190}}>
+                            <Radio onChange={value => this.config.saveType = value} defaultValue={saveType} options={[
+                                {label: '本地', value: '0'},
+                                // {label: '服务端', value: '1'},
+                            ]}/>
+                        </ConfigItem>
+                        <ConfigItem title={'草稿状态数据实时刷新'} contentStyle={{width: 190}}>
+                            <LcSwitch defaultValue={realTimeRefresh}
+                                      onChange={value => this.config.realTimeRefresh = value}/>
+                        </ConfigItem>
+                    </div>
+                    <p style={{padding: '5px 7px', color: '#989898'}}>说明：数据实时刷新开启后，草稿状态下组件数据也会根据配置实时更新</p>
+                    <div className={'lc-header-project-footer'}>
+                        <LcButton type={'submit'}>保存</LcButton>
+                        <LcButton type={'button'} onClick={this.onClose}>取消</LcButton>
+                    </div>
+                </form>
             </Dialog>
         );
     }
