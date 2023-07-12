0eb7e460970d97f0709edb00b2aae2aae529dff3
xiaopu <1182810784@qq.com>
2023-07-12 10:59:03 +0800

    本地存储数据结构调整

--- src/framework/operate/LocalOperator.ts
+++ src/framework/operate/LocalOperator.ts
@@ -8,6 +8,7 @@ import {buildUrlParams, parseUrlParams} from "../../utils/URLUtil";
 import {AbstractOperator} from "./AbstractOperator";
 import {idGenerate} from "../../utils/IdGenerate";
 import {LocalConstant} from "../LocalConstant";
+import {message} from "antd";
 
 /**
  * 本地项目数据操作实现
@@ -27,23 +28,32 @@ class LocalOperator extends AbstractOperator {
     }
 
     private static async createProject(projectData: ProjectDataType): Promise<string> {
+        let time = new Date().getTime();
         await LocalOperator.createProjectBefore(projectData);
         await LocalOperator.doCreate(projectData);
         LocalOperator.createProjectAfter(projectData);
+        console.log('createProject time', new Date().getTime() - time);
         return projectData!.id || '';
     }
 
     private static async createProjectBefore(projectData: ProjectDataType): Promise<void> {
         projectData.id = idGenerate.generateId();
+        const {maxLevel, minLevel} = eventOperateStore;
+        projectData.layerConfigs!.maxLevel = maxLevel;
+        projectData.layerConfigs!.minLevel = minLevel;
         // 2. 对项目画面截图
+        let time = new Date().getTime();
         await LocalOperator.doScreenshot(projectData);
+        console.log('doScreenshot time', new Date().getTime() - time);
         // 3. 如果有背景图片则处理背景图片
         const bgConfig: BackgroundConfig = (projectData.elemConfigs ?? {})['-1']['background'];
         if (bgConfig?.bgImg.bgImgUrl !== '') {
             //4. 生成背景图片的key
             const bgImgKey = LocalConstant.LOCAL_BACKGROUND_IMG + projectData.id;
             //5. 保存背景图片到本地数据库
+            let time = new Date().getTime();
             const res = await ImgUtil.saveImgToLocal(bgConfig.bgImg.bgImgUrl!, bgImgKey);
+            console.log('doScreenshot time', new Date().getTime() - time);
             //6. 将背景图片的可访问地址替换为唯一标识。
             if (res) bgConfig.bgImg.bgImgUrl = bgImgKey;
         }
@@ -52,7 +62,7 @@ class LocalOperator extends AbstractOperator {
     private static async doCreate(projectData: ProjectDataType): Promise<void> {
         try {
             if (!projectData.id)
-                throw new Error('project id is null');
+                throw new Error('创建项目id异常');
             await localforage.setItem(projectData.id, projectData);
             // 8. 维护项目列表（保存项目的轻量级描述信息，避免加载列表时内存占用过大）
             await LocalOperator.doSaveProjectSimpleInfo(projectData);
@@ -70,17 +80,20 @@ class LocalOperator extends AbstractOperator {
         let urlParams = parseUrlParams();
         urlParams = {...urlParams, ...{id, action: 'edit'}};
         window.history.replaceState(null, '', '?' + buildUrlParams(urlParams));
-        alert("create success");
+        message.success('创建成功');
     }
 
     private async updateProject(projectData: ProjectDataType): Promise<boolean> {
         await LocalOperator.updateProjectBefore(projectData);
         await this.doUpdate(projectData);
-        alert('update success');
+        message.success('保存成功');
         return true;
     }
 
     private static async updateProjectBefore(projectData: ProjectDataType): Promise<void> {
+        const {maxLevel, minLevel} = eventOperateStore;
+        projectData.layerConfigs!.maxLevel = maxLevel;
+        projectData.layerConfigs!.minLevel = minLevel;
         //屏幕截图
         await LocalOperator.doScreenshot(projectData);
         //处理背景图片
@@ -111,7 +124,7 @@ class LocalOperator extends AbstractOperator {
 
     private static async doSaveProjectSimpleInfo(projectData: any): Promise<void> {
         // 8. 维护项目列表（保存项目的轻量级描述信息，避免加载列表时内存占用过大）
-        const simpleInfoList = await localforage.getItem('lc_project_list');
+        const simpleInfoList = await localforage.getItem(LocalConstant.LOCAL_SIMPLE_PROJECT_LIST);
         const {id, projectConfig: {name, des, state, updateTime, screenshot, saveType}} = projectData;
         const simpleData = {id, name, des, state, updateTime, screenshot, saveType};
         if (simpleInfoList && Array.isArray(simpleInfoList)) {
@@ -141,9 +154,6 @@ class LocalOperator extends AbstractOperator {
     }
 
     private static async doScreenshot(projectData: ProjectDataType): Promise<void> {
-        const {maxLevel, minLevel} = eventOperateStore;
-        projectData.layerConfigs!.maxLevel = maxLevel;
-        projectData.layerConfigs!.minLevel = minLevel;
         let imgDom: any = document.querySelector('.lc-content-scale');
         const screenShotId = LocalConstant.LOCAL_PROJECT_SCREENSHOT + projectData.id;
         const res = await ImgUtil.htmlToImgWithId(imgDom, screenShotId, {scale: scaleCore.scale});
@@ -153,6 +163,7 @@ class LocalOperator extends AbstractOperator {
 
     public async getProjectSimpleInfoList(): Promise<any[]> {
         let simpleDataList = await localforage.getItem(LocalConstant.LOCAL_SIMPLE_PROJECT_LIST);
+        console.log('simpleDataList', simpleDataList);
         if (simpleDataList && simpleDataList instanceof Array)
             return simpleDataList;
         else
