f1d0f7990a5c12c4b99dad6c3b1e36483470ee00
xiaopu <1182810784@qq.com>
2023-09-06 21:36:30 +0800

    验证vscode编辑器

--- src/test/MonacoDemo.tsx
+++ src/test/MonacoDemo.tsx
@@ -1,24 +1,33 @@
 import React, {Component} from 'react';
-import * as monaco from 'monaco-editor';
+import Editor from "@monaco-editor/react";
 
 class MonacoDemo extends Component {
 
     monacoContainer: any = null;
 
-    componentDidMount(): void {
-        monaco.editor.create(this.monacoContainer, {
-            value: "function foo() {\n console.log('foo');\n}",
-            theme: 'vs-dark',
-            minimap: {
-                enabled: false,
-            },
-            language: 'javascript',
-        });
-    }
+    // componentDidMount(): void {
+    //     monaco.editor.create(this.monacoContainer, {
+    //         value: "function foo() {\n console.log('foo');\n}",
+    //         theme: 'vs-dark',
+    //         minimap: {
+    //             enabled: false,
+    //         },
+    //         language: 'javascript',
+    //         quickSuggestions: false,
+    //     });
+    // }
 
     render() {
         return (
-            <div id={'monaco_demo'} ref={ref => this.monacoContainer = ref} style={{width: '100%', height: '100%'}}/>
+            <Editor
+                height="90vh"
+                defaultLanguage="javascript"
+                defaultValue="function foo() {
+                 console.log('foo');
+                 }"
+                theme="vs-dark"
+                // onMount={handleEditorDidMount}
+            />
         );
     }
 }
