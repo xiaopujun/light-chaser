ef696d4c20d09f5f4cc9aa905ac205c7570cb28a
xiaopu <1182810784@qq.com>
2023-11-02 15:05:21 +0800

    修复删除蓝图节点时，误删画布中组件的bug

--- src/designer/header/items/blue-print/BluePrintHdItem.ts
+++ src/designer/header/items/blue-print/BluePrintHdItem.ts
@@ -1,6 +1,7 @@
 import {AbstractHeaderItem, HeaderItemProps} from "../../HeaderTypes";
 import headerStore from "../../HeaderStore";
 import {CodeSandboxSquareFilled} from "@ant-design/icons";
+import eventOperateStore from "../../../operate-provider/EventOperateStore";
 
 export default class BluePrintHdItem extends AbstractHeaderItem {
     getHeaderItemInfo(): HeaderItemProps {
@@ -10,6 +11,9 @@ export default class BluePrintHdItem extends AbstractHeaderItem {
             name: '蓝图交互',
             order: 0,
             onClick: () => {
+                //打开蓝图前清空画布中已经选中的组件,避免删除蓝图节点时，误删画布中的组件
+                const {setTargets} = eventOperateStore;
+                setTargets([]);
                 setBluePrintVisible(true);
             }
         };
