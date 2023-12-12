af0e80c2a43f71d1da82c3ea07351b82896c7628
xiaopu <1182810784@qq.com>
2023-12-12 20:16:05 +0800

    color block组件调整为使用hook组件形式

--- src/comps/lc/base-color-block/BaseColorBlockController.ts
+++ src/comps/lc/base-color-block/BaseColorBlockController.ts
@@ -2,15 +2,18 @@ import {ThemeItemType} from "../../../designer/DesignerType";
 import {UpdateOptions} from "../../../framework/core/AbstractController";
 import AbstractDesignerController from "../../../framework/core/AbstractDesignerController";
 import ComponentUtil from "../../../utils/ComponentUtil";
-import BaseColorBlockComponent, {BaseColorBlockComponentProps} from "./BaseColorBlockComponent";
+import BaseColorBlockComponent, {
+    BaseColorBlockComponentProps,
+    BaseColorBlockComponentRef
+} from "./BaseColorBlockComponent";
 import ObjectUtil from "../../../utils/ObjectUtil";
 
-export class BaseColorBlockController extends AbstractDesignerController<BaseColorBlockComponent, BaseColorBlockComponentProps> {
+export class BaseColorBlockController extends AbstractDesignerController<BaseColorBlockComponentRef, BaseColorBlockComponentProps> {
 
     create(container: HTMLElement, config: any): void {
         this.config = config;
         this.container = container;
-        ComponentUtil.createAndRender<BaseColorBlockComponent>(container, BaseColorBlockComponent, config).then((instance) => this.instance = instance);
+        ComponentUtil.createAndRender<BaseColorBlockComponentRef>(container, BaseColorBlockComponent, config).then((instance) => this.instance = instance);
     }
 
     destroy(): void {
@@ -26,7 +29,7 @@ export class BaseColorBlockController extends AbstractDesignerController<BaseCol
         this.config = ObjectUtil.merge(this.config, config);
         upOp = upOp || {reRender: true};
         if (upOp.reRender)
-            this.instance?.setState(this.config);
+            this.instance?.updateConfig(this.config!);
     }
 
     updateTheme(newTheme: ThemeItemType): void {
