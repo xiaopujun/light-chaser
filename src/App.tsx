1dbcd1567519089ff8b6ae3b9a2c5a4ba65c67f4
xiaopu <1182810784@qq.com>
2023-12-19 19:39:43 +0800

    调整组件样式和结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import {Component, lazy, Suspense} from 'react';
-import 'antd/dist/reset.css';
+import {lazy, Suspense} from 'react';
 import './App.less';
 import {Route, Routes} from "react-router-dom";
 import Loading from "./ui/loading/Loading";
+import {ConfigProvider, theme} from "antd";
 
 const DemoMain = lazy(() => import('./test/DemoMain'));
 const Login = lazy(() => import('./pages/login/Login').then(module => ({default: module.Login})));
@@ -10,23 +10,19 @@ const Designer = lazy(() => import('./designer/Designer'));
 const DesignerView = lazy(() => import('./designer/view/DesignerView'));
 const Home = lazy(() => import('./pages/home/Home'));
 
-class App extends Component {
-    render() {
-        return (
-            <>
-                <Suspense fallback={<Loading/>}>
-                    <Routes>
-                        <Route path={'/designer'} element={<Designer/>}/>
-                        <Route path={'/view'} element={<DesignerView/>}/>
-                        <Route path={'/test'} element={<DemoMain/>}/>
-                        <Route path={'/login'} element={<Login/>}/>
-                        <Route path={'/home'} element={<Home/>}/>
-                        <Route path={'/'} element={<Login/>}/>
-                    </Routes>
-                </Suspense>
-            </>
-        );
-    }
+export default function App() {
+    return (
+        <ConfigProvider theme={{algorithm: theme.darkAlgorithm}}>
+            <Suspense fallback={<Loading/>}>
+                <Routes>
+                    <Route path={'/designer'} element={<Designer/>}/>
+                    <Route path={'/view'} element={<DesignerView/>}/>
+                    <Route path={'/test'} element={<DemoMain/>}/>
+                    <Route path={'/login'} element={<Login/>}/>
+                    <Route path={'/home'} element={<Home/>}/>
+                    <Route path={'/'} element={<Login/>}/>
+                </Routes>
+            </Suspense>
+        </ConfigProvider>
+    );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {createRoot} from "react-dom/client";
+import 'antd/dist/reset.css';
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
