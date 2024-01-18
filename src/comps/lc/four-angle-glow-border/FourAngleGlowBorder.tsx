7418226b4f09f94aa0b354e6237ad26368ae91c2
xiaopu <1182810784@qq.com>
2024-01-18 16:31:14 +0800

    新增四角辉光边框组件

---
+++ src/comps/lc/four-angle-glow-border/FourAngleGlowBorder.tsx
@@ -0,0 +1,51 @@
+import './FourAngleGloeBorder.less';
+import {ForwardedRef, forwardRef, useEffect, useImperativeHandle, useRef, useState} from "react";
+import {FourAngleGlowProps} from "./FourAngleGlowBorderController";
+
+export interface FourAngleGlowBorderStyle {
+    width?: number;
+    length?: number;
+    color?: string;
+    radius?: number;
+}
+
+export interface FourAngleGlowBorderRef {
+    updateConfig: (config: FourAngleGlowBorderStyle) => void;
+    setEventHandler: (eventMap: Record<string, Function>) => void;
+}
+
+const FourAngleGlowBorder = forwardRef((props: FourAngleGlowProps, ref: ForwardedRef<FourAngleGlowBorderRef>) => {
+    const [config, setConfig] = useState<FourAngleGlowProps>({...props});
+    const fourAngleGlowRef = useRef<HTMLDivElement | null>(null);
+    const eventHandlerMap = useRef<Record<string, Function>>({});
+
+
+    const updateStyle = (config: FourAngleGlowBorderStyle) => {
+        fourAngleGlowRef.current?.style.setProperty('--fagb-length', `${config?.length}px`);
+        fourAngleGlowRef.current?.style.setProperty('--fagb-color', config!.color!);
+        fourAngleGlowRef.current?.style.setProperty('--fagb-radius', `${config?.radius}px`);
+        fourAngleGlowRef.current?.style.setProperty('--fagb-width', `${config?.width}px`);
+    }
+
+    useImperativeHandle(ref, () => ({
+        updateConfig: (newConfig) => updateStyle({...newConfig}),
+        setEventHandler: (eventMap) => eventHandlerMap.current = eventMap,
+    }));
+
+    useEffect(() => {
+        updateStyle(config.style!);
+    });
+
+    return (
+        <div className="four-angle-glow" ref={fourAngleGlowRef}>
+            <span className="angle angle-tl"/>
+            <span className="angle angle-tr"/>
+            <span className="angle angle-bl"/>
+            <span className="angle angle-br"/>
+        </div>
+    );
+})
+
+export default FourAngleGlowBorder;
+
+
--- src/test/Demo.tsx
+++ src/test/Demo.tsx
@@ -1,29 +1,9 @@
 import './DemoMain.less';
-import {Control} from "../json-schema/SchemaTypes";
-import {FieldChangeData, LCGUI} from "../json-schema/LCGUI";
 
 export default function Demo() {
-
-    const schema: Control = {
-        type: 'card-panel',
-        label: '卡片面板',
-        tip: '这是一个卡片面板',
-        children: [
-            {
-                type: 'input',
-                label: '输入框',
-                tip: '这是一个输入框',
-            }
-        ]
-    }
-
-    const onFieldChange = (fieldChangeData: FieldChangeData) => {
-        console.log(fieldChangeData);
-    }
-
     return (
-        <div style={{width: 400, padding: 10, background: '#313131'}}>
-            <LCGUI schema={schema} onFieldChange={onFieldChange}/>
+        <div style={{width: 550, height: 200, padding: 10}}>
+
         </div>
     )
 }
