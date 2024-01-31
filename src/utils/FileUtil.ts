5ce1cb6b6c83992d9cc9ac039d1c84f53c06de70
dagu <1182810784@qq.com>
2024-01-31 12:30:06 +0800

    缩减本地上传图片，校验图片重复所需的hash码长度，由64，调整为16位

--- src/utils/FileUtil.ts
+++ src/utils/FileUtil.ts
@@ -4,6 +4,6 @@ export default class FileUtil {
         const hashArray = await crypto.subtle.digest('SHA-256', buffer);
         return Array.from(new Uint8Array(hashArray))
             .map(b => b.toString(16).padStart(2, '0'))
-            .join('');
+            .join('').substring(0, 16); //直接截取16位hash值可能会增加hash碰撞的风险。此时需要尤其关注使用情况
     }
 }
