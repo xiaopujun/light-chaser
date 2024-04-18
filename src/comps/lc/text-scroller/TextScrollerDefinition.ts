6d07ee676bd5fb042bc2dd190e6ee051337e588a
dagu <1182810784@qq.com>
2024-04-18 15:18:27 +0800

    初步实现文字弹幕组件

---
+++ src/comps/lc/text-scroller/TextScrollerDefinition.ts
@@ -0,0 +1,82 @@
+import {
+    AbstractDefinition,
+    BaseInfoType,
+    EventInfo,
+    MenuToConfigMappingType
+} from "../../../framework/core/AbstractDefinition";
+import {ClazzTemplate} from "../../common-component/common-types";
+import {MenuInfo} from "../../../designer/right/MenuType";
+import textScrollerImg from './text-scroller.png';
+import {getDefaultMenuList} from "../../../designer/right/util";
+import {TextScrollerController} from "./TextScrollerController.ts";
+import {TextScrollerComponentProps} from "./TextScrollerComponent.tsx";
+import BaseInfo from "../../common-component/base-info/BaseInfo";
+import AnimationConfig from "../../common-component/animation-config/AnimationConfig";
+import ThemeConfig from "../../common-component/theme-config/ThemeConfig";
+import {TextScrollerConfig} from "./TextScrollerConfig.tsx";
+import DataConfig from "../../common-component/data-config/DataConfig.tsx";
+
+export default class TextScrollerDefinition extends AbstractDefinition<TextScrollerController, TextScrollerComponentProps> {
+    getBaseInfo(): BaseInfoType {
+        return {
+            compName: "文字弹幕",
+            compKey: "TextScroller",
+            categorize: "info",
+            subCategorize: "text",
+        };
+    }
+
+    getChartImg(): string | null {
+        return textScrollerImg;
+    }
+
+    getController(): ClazzTemplate<TextScrollerController> | null {
+        return TextScrollerController;
+    }
+
+    getInitConfig(): TextScrollerComponentProps {
+        return {
+            base: {
+                id: "",
+                name: '文字弹幕',
+                type: 'TextScroller',
+            },
+            style: {
+                speed: 10,
+                fontSize: 16,
+                fontWeight: 500,
+                color: '#fff',
+                fontFamily: 'Arial',
+            },
+            data: {
+                sourceType: 'static',
+                staticData: '文字弹幕'
+            }
+        };
+    }
+
+    getMenuList(): Array<MenuInfo> | null {
+        return getDefaultMenuList().filter((item: MenuInfo) => (item.key !== 'theme' && item.key !== 'mapping'));
+    }
+
+    getMenuToConfigContentMap(): MenuToConfigMappingType | null {
+        return {
+            base: BaseInfo,
+            style: TextScrollerConfig,
+            animation: AnimationConfig,
+            data: DataConfig,
+            theme: ThemeConfig
+        };
+    }
+
+
+    getEventList(): EventInfo[] {
+        const events = super.getEventList();
+        return events.concat([
+            {
+                id: "click",
+                name: "点击时",
+            }
+        ]);
+    }
+}
--- src/test/Demo.tsx
+++ src/test/Demo.tsx
@@ -1,13 +1,20 @@
 import './DemoMain.less';
-import MonacoEditor from "../json-schema/ui/code-editor/MonacoEditor.tsx";
+import {useEffect} from "react";
 
 
 export default function Demo() {
+    useEffect(() => {
+
+    }, []);
 
     return (
-        <div style={{width: 400}}>
-            <MonacoEditor value={'select * from table;'}
-                          language={'sql'} width={400} height={200}/>
+        <div style={{width: 500, height: 200}}>
+            <div className="text-scroller-container">
+                <div className="text-scroller">
+                    数据库的发的是开发商的交锋是点击发送的
+                </div>
+            </div>
         </div>
+
     );
 }
