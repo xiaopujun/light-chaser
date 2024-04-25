bb0ad6d2f04f19e952a954f843a100a305c1c59f
dagu <1182810784@qq.com>
2024-04-25 17:41:17 +0800

    新建项目使用默认值1920 * 1080

--- src/pages/home/project-list/AddNewProjectDialog.tsx
+++ src/pages/home/project-list/AddNewProjectDialog.tsx
@@ -22,10 +22,10 @@ interface AddNewScreenDialogProps {
 export const AddNewProjectDialog: React.FC<AddNewScreenDialogProps> = (props) => {
 
     const projectInfo: INewProjectInfo = {
-        name: '',
+        name: '未命名项目',
         des: '',
-        width: 500,
-        height: 300,
+        width: 1920,
+        height: 1080,
     }
 
     const onOk = (e: FormEvent<HTMLFormElement>) => {
@@ -45,13 +45,13 @@ export const AddNewProjectDialog: React.FC<AddNewScreenDialogProps> = (props) =>
             <form onSubmit={onOk}>
                 <div className={'lc-add-new-screen'}>
                     <Grid gridGap={'15px'} columns={2}>
-                        <Input label={'名称'} maxLength={20}
+                        <Input label={'名称'} maxLength={20} defaultValue={projectInfo.name}
                                onChange={(name: string | number) => projectInfo.name = name as string}/>
                         <Input label={'描述'} maxLength={20}
                                onChange={(description: string | number) => projectInfo.des = description as string}/>
-                        <NumberInput label={'宽度'} min={300}
+                        <NumberInput label={'宽度'} min={300} defaultValue={projectInfo.width}
                                      onChange={(width: string | number) => projectInfo.width = Number(width)}/>
-                        <NumberInput label={'高度'} min={300}
+                        <NumberInput label={'高度'} min={300} defaultValue={projectInfo.height}
                                      onChange={(height: number | string) => projectInfo.height = Number(height)}/>
                     </Grid>
                 </div>
