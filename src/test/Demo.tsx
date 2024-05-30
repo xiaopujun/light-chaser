a2e296b66f604cda38d0d828c68c99385b2159e9
dagu <1182810784@qq.com>
2024-05-30 12:13:29 +0800

    修复demo编译错误

--- src/test/Demo.tsx
+++ src/test/Demo.tsx
@@ -1,6 +1,7 @@
 import './DemoMain.less';
 import "@amap/amap-jsapi-types";
 import ProjectItem from "../pages/home/project-list/ProjectItem.tsx";
+import {SaveType} from "../designer/DesignerType.ts";
 
 
 export default function Demo() {
@@ -8,6 +9,7 @@ export default function Demo() {
     return (
         <ProjectItem id={'22'} name={'使得开发商积分'} doOperate={(id: string, type: string) => {
         }}
+                     saveType={SaveType.LOCAL}
                      cover={'blob:http://localhost:5173/9fdc377b-540b-4fa2-a9fd-8d33b6b5735b'}/>
     );
 }
