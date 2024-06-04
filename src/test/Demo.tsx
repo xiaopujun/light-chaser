ff6321e900e10e341a936d2493b04655b0c46a42
dagu <1182810784@qq.com>
2024-06-04 20:23:36 +0800

    消除编译错误

--- src/test/Demo.tsx
+++ src/test/Demo.tsx
@@ -1,13 +1,10 @@
 import './DemoMain.less';
 import "@amap/amap-jsapi-types";
-import ProjectItem from "../pages/home/project-list/ProjectItem.tsx";
 
 
 export default function Demo() {
 
     return (
-        <ProjectItem id={'22'} name={'使得开发商积分'} doOperate={(id: string, type: string) => {
-        }}
-                     cover={'blob:http://localhost:5173/9fdc377b-540b-4fa2-a9fd-8d33b6b5735b'}/>
+        <div></div>
     );
 }
