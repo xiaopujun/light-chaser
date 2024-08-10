c8fc2eeb2e378f78745a91d96208e14388a19906
dagu <1182810784@qq.com>
2024-08-10 10:30:21 +0800

    处理颜色选择器bug

--- src/json-schema/ui/color-picker/ColorPicker.tsx
+++ src/json-schema/ui/color-picker/ColorPicker.tsx
@@ -1,8 +1,6 @@
 import {ColorPicker as AntdColorPicker} from 'antd';
 import './ColorPicker.less';
 import {UIContainer, UIContainerProps} from "../ui-container/UIContainer";
-import {useState} from "react";
-import {Color} from "antd/es/color-picker";
 
 interface ColorPickerProps extends UIContainerProps {
     value?: string;
@@ -12,18 +10,12 @@ interface ColorPickerProps extends UIContainerProps {
     onChange?: (color: string) => void;
 }
 
-
 export default function ColorPicker(props: ColorPickerProps) {
     const {value, defaultValue, showText, disabled, onChange, ...containerProps} = props;
-    const controlled = value !== undefined && defaultValue === undefined;
-    const [stateValue, setStateValue] = useState(controlled ? value : defaultValue);
-    const finalValue = controlled ? value : stateValue;
 
-    const onChangeComplete = (color: Color) => {
+    const _onChange = (color: any) => {
         const value = color.toHexString();
         onChange && onChange(value);
-        if (!controlled)
-            setStateValue(value);
     };
 
     return (
@@ -32,55 +24,11 @@ export default function ColorPicker(props: ColorPickerProps) {
                 size={'small'}
                 format={'hex'}
                 disabled={disabled}
-                value={finalValue}
-                onChangeComplete={onChangeComplete}
+                value={value}
+                defaultValue={defaultValue}
+                showText={showText}
+                onChange={_onChange}
             />
-            {showText && <span className={'text-area'}>{finalValue?.toUpperCase()}</span>}
         </UIContainer>
     );
 }
-
-
-// class ColorPicker extends Component<ColorPickerProps> {
-//
-//     control: boolean = true;
-//
-//     state = {
-//         value: ''
-//     }
-//
-//     constructor(props: ColorPickerProps) {
-//         super(props);
-//         const {value, defaultValue} = props;
-//         this.control = !defaultValue && !!value;
-//         this.state = {value: defaultValue || value || '#00e9ff'};
-//     }
-//
-//     onChangeComplete = (color: Color) => {
-//         const {onChange} = this.props;
-//         const value = color.toHexString();
-//         onChange && onChange(value);
-//         if (!this.control) {
-//             this.setState({value});
-//         }
-//     };
-//
-//     render() {
-//         const color = this.control ? this.props.value : this.state.value;
-//         const {disabled, tip, label, showText} = this.props;
-//         return (
-//             <UIContainer tip={tip} label={label} className={'lc-color-pick'}>
-//                 <AntdColorPicker
-//                     size={'small'}
-//                     format={'hex'}
-//                     disabled={disabled}
-//                     value={color}
-//                     onChangeComplete={this.onChangeComplete}
-//                 />
-//                 {showText && <span className={'text-area'}>{color?.toUpperCase()}</span>}
-//             </UIContainer>
-//         );
-//     }
-// }
-//
-// export default ColorPicker;
