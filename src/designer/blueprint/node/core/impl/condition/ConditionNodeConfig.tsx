857a82736aed13b98581b5ee1926829a73e6ddbe
dagu <1182810784@qq.com>
2024-08-23 20:15:58 +0800

    修复蓝图节点配置丢失的bug

--- src/designer/blueprint/node/core/impl/condition/ConditionNodeConfig.tsx
+++ src/designer/blueprint/node/core/impl/condition/ConditionNodeConfig.tsx
@@ -1,4 +1,4 @@
-import React, {Suspense} from "react";
+import React, {Suspense, useState} from "react";
 import {BPRightConfigProps} from "../../../../right/BPRight";
 import {debounce} from "lodash";
 import Loading from "../../../../../../json-schema/ui/loading/Loading.tsx";
@@ -9,9 +9,11 @@ export const ConditionNodeConfig: React.FC<BPRightConfigProps> = (props) => {
     const {controller} = props;
     const config = controller?.getConfig();
     const handleCode = config.handler || "function handler(data) { \n \n }";
+    const [count, setCount] = useState(0);
     const onCodeChange = debounce((value: string | undefined) => {
         if (!value) return;
         controller?.update({handler: value});
+        setCount(count + 1);
     }, 500);
     return (
         <div className={'condition-node-config'}>
