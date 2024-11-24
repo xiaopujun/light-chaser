ee7dfbcd977e4f46d7690821cf52dbc4b8fbec22
dagu <1182810784@qq.com>
2024-11-24 16:29:04 +0800

    优化登录页面，logo信息

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,11 @@
 import './Login.less';
-import Input from "../../json-schema/ui/input/Input";
-import Button from "../../json-schema/ui/button/Button";
+import {Button, Input} from 'antd';
 import {globalMessage} from "../../framework/message/GlobalMessage";
 import {useNavigate} from "react-router-dom";
 import {memo} from "react";
+import logo from './login-logo.png';
+import user from './user.svg';
+import lock from './lock.svg';
 
 const Login = memo(() => {
 
@@ -23,25 +25,26 @@ const Login = memo(() => {
     return (
         <div className={'lc-login'}>
             <div className={'lc-login-container'}>
-                <div className={'lc-login-left'}/>
-                <div className={'lc-login-right'}>
-                    <div className={'login-header'}>
-                        <p className={'header-title'}>LIGHT CHASER</p>
-                        <p className={'header-des'}>快捷，高效的数据可视化设计工具</p>
+                <div className={'login-header'}>
+                    <p className={'header-logo'}><img src={logo} alt={'logo'}/></p>
+                    <p className={'header-des'}>快捷，高效的数据可视化设计工具</p>
+                </div>
+                <div className={'login-body'}>
+                    <div className={'login-item'}>
+                        <Input prefix={<img alt={'user'} src={user}/>} size={'large'}
+                               onChange={e => account = e.target.value}
+                               placeholder={'用户名'}
+                               defaultValue={account}/>
                     </div>
-                    <div className={'login-body'}>
-                        <div className={'login-item'}>
-                            <Input onChange={value => account = value as string}
-                                   placeholder={'用户名'}
-                                   defaultValue={account}/>
-                        </div>
-                        <div className={'login-item'}>
-                            <Input onChange={value => password = value as string}
-                                   defaultValue={password}
-                                   placeholder={'密码'}
-                                   type={'password'}/>
-                        </div>
-                        <div className={'login-item'}><Button onClick={login}>登录</Button></div>
+                    <div className={'login-item'}>
+                        <Input prefix={<img alt={'lock'} src={lock}/>} size={'large'}
+                               onChange={e => password = e.target.value}
+                               placeholder={'密码'}
+                               defaultValue={password}
+                               type={'password'}/>
+                    </div>
+                    <div className={'login-item'}><Button size={'large'} style={{width: '100%'}} type="primary"
+                                                          onClick={login}>登录</Button>
                     </div>
                 </div>
             </div>
