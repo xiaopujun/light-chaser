fe2d8aeec41f827b687cc3e2d4c8117401200e9e
马军 <majun@xiwicloud.com>
2025-02-16 17:53:38 +0800

    fix: 改点配置

--- vite.dev.config.ts
+++ vite.dev.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/light-chaser-app/',
     server: {
         proxy: {
             '^/(api|static)/.*': {
