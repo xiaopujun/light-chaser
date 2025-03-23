bdd1a772dbee197a4e458d191fead695469b12a7
dagu <1182810784@qq.com>
2025-03-23 12:13:55 +0800

    增加版权声明

--- src/comps/antd/bar-range/AntdRangeBarDefinition.ts
+++ src/comps/antd/bar-range/AntdRangeBarDefinition.ts
@@ -1,3 +1,14 @@
+/*
+ * Copyright © 2023-2025 puyinzhen
+ * All rights reserved.
+ *
+ * The copyright of this work (or idea/project/document) is owned by puyinzhen. Without explicit written permission, no part of this work may be reproduced, distributed, or modified in any form for commercial purposes.
+ *
+ * This copyright statement applies to, but is not limited to: concept descriptions, design documents, source code, images, presentation files, and any related content.
+ *
+ * For permission to use this work or any part of it, please contact 1182810784@qq.com to obtain written authorization.
+ */
+
 import rangeBarImg from "./range-bar.png";
 import {AntdBarProps} from "../../antd-common/bar/AntdCommonBarController";
 import AbstractBarDefinition from "../../antd-common/bar/AbstractBarDefinition";
--- src/global/GlobalConstants.ts
+++ src/global/GlobalConstants.ts
@@ -1,3 +1,14 @@
+/*
+ * Copyright © 2023-2025 puyinzhen
+ * All rights reserved.
+ *
+ * The copyright of this work (or idea/project/document) is owned by puyinzhen. Without explicit written permission, no part of this work may be reproduced, distributed, or modified in any form for commercial purposes.
+ *
+ * This copyright statement applies to, but is not limited to: concept descriptions, design documents, source code, images, presentation files, and any related content.
+ *
+ * For permission to use this work or any part of it, please contact 1182810784@qq.com to obtain written authorization.
+ */
+
 export const LIGHT_CHASER_PROJECT_LIST = 'light_chaser_project_list';
 
 export const IMAGE_SOURCE_PREFIX = 'image_source_';
